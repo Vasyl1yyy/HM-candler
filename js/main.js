daa95a7d57172715c47fa9c148a465a1668e0636
Vasyl1yyy <115637018+Vasyl1yyy@users.noreply.github.com>
2023-09-11 20:01:10 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@ const menuMobil = document.querySelector('.menu__list');
 menuBtn.addEventListener('click', () => {
   menuMobil.classList.toggle('menu--open')
 })
+
+const swiperOne = new Swiper('.feedback__slider', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  }
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
