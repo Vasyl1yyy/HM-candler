45ceb315415c5bb2e0616474c94fecaf3ff52a25
Vasyl1yyy <115637018+Vasyl1yyy@users.noreply.github.com>
2023-10-15 14:33:06 +0300

    close

--- js/main.js
+++ js/main.js
@@ -9,14 +9,34 @@ const swiperOne = new Swiper('.feedback__slider', {
   loop: true,
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   }
 });
 
 const swiperTwo = new Swiper('.certificates__slider', {
-  slidesPerView: 2,
+  slidesPerView: 3,
   spaceBetween: 20,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
 });
+
+
+const accordeon = document.querySelector('.accordeon');
+const accordeonTitles = accordeon.querySelectorAll('.accordeon__title');
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
+  accordeonTitle.addEventListener('click', function () {
+    const currentText = accordeonTitle.parentElement.querySelector('.accordeon__text');
+
+    accordeonTitle.classList.toggle('accordeon__title--active');
+    currentText.classList.toggle('accordeon__text--visible');
+
+    if (currentText.classList.contains('accordeon__text--visible')) {
+      currentText.style.maxHeight = currentText.scrollHeight + 'px'
+    } else {
+      currentText.style.maxHeight = null;
+    }
+  })
+})
